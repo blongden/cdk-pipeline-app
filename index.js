5be7c752f61890b165affd65ebcc05ebb5d70338
Ben Longden <ben.longden@infinityworks.com>
2023-12-12 15:49:53 +0000

    add a test for greeting

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+function greeting() {
+  return "Hello, world!"
+}
+
 exports.handler = async (event) => {
   const response = {
     statusCode: 200,
-    body: JSON.stringify("Hello, world!"),
+    body: JSON.stringify(greeting()),
   }
   return response
 }
+
+exports.greeting = greeting
