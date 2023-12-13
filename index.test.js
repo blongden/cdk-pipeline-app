1917c86409650afb542e55d9ea577f30aa8e0cc1
Ben Longden <ben.longden@infinityworks.com>
2023-12-13 10:39:01 +0000

    Fix the build

--- index.test.js
+++ index.test.js
@@ -3,5 +3,5 @@ const index = require("./index")
 test('handler returns expected response', async () => {
     const response = await index.handler()
     expect(response.statusCode).toBe(200)
-    expect(JSON.parse(response.body)).toBe("Hello, broken world!")
+    expect(JSON.parse(response.body)).toBe("Hello, world!")
 })
